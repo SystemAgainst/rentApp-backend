007cab5da29e74df1d7af3877eedd160daf66f6d
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-05-06 19:48:38 +0300

    add: added constants

--- errors/constants.js
+++ errors/constants.js
@@ -1,1 +1,5 @@
 module.exports.NOT_FOUND_ID = 'Не задан Id';
+module.exports.INVALID_DATA = 'Некорректные имя пользователя и/или пароль';
+module.exports.USER_NOT_FOUND = 'Пользователь не найден';
+module.exports.EMAIL_EXIST = 'Пользователь не найден';
+module.exports.PASSPORT_ALREADY_EXIST = 'ППользователь с таким passport_id уже существует';
